27d649286cb54c7ebce82bdf106467593bbe1451
Rafael Peratello <rafael@openship.com.br>
2015-12-14 19:38:03 -0200

    migration of table to add birthday

--- app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
@@ -12,8 +12,10 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     public static final String KEY_NAME = "nome";
     public static final String KEY_FONE = "fone";
     public static final String KEY_FONE2 = "fone2";
+    public static final String KEY_BIRTHDAY = "aniversario";
     public static final String KEY_EMAIL = "email";
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
+
     public static final String DATABASE_CREATE = "CREATE TABLE " + DATABASE_TABLE + " (" +
             KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             KEY_NAME + " TEXT NOT NULL, " +
@@ -39,7 +41,15 @@ public class SQLiteHelper extends SQLiteOpenHelper {
                 Log.d("Agenda", DATABASE_UPDATE_1);
 
                 database.execSQL(DATABASE_UPDATE_1);
-                break;
+
+            case 2:
+                Log.d("Agenda", "Update da versao 2 para 3");
+
+                String DATABASE_UPDATE_2 = "ALTER TABLE " + DATABASE_TABLE + " ADD " + KEY_BIRTHDAY + " TEXT";
+                Log.d("Agenda", DATABASE_UPDATE_2);
+
+                database.execSQL(DATABASE_UPDATE_2);
+
         }
     }
 }
